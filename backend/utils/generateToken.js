2bc4707f6a405f1c0d69b84266ac0cb14046aa1f
Nilesh Wani <108217005+nileshnwani@users.noreply.github.com>
2025-04-06 02:36:57 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,13 +6,10 @@ export const generateToken = (req, res, userId) => {
     expiresIn: req.body.remember ? 365 * 24 + 'h' : '24h'
   });
 
-  // Setting the JWT as an HTTP-only cookie for enhanced security
-res.cookie('jwt', token, {
-  httpOnly: true,              // Prevents JavaScript access to the cookie
-  secure: true,                // Ensures it's only sent over HTTPS
-  sameSite: 'strict',          // Blocks cross-site cookie usage (strong security)
-  maxAge: req.body.remember 
-         ? 365 * 24 * 60 * 60 * 1000  // 1 year in ms
-         : 24 * 60 * 60 * 1000        // 1 day in ms
-});
-
+ // Setting the JWT as an HTTP-only cookie for enhanced security
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'strict',
+    maxAge: req.body.remember ? 365 * 24 * 60 * 60 * 1000 : 24 * 60 * 60 * 1000
+  });
